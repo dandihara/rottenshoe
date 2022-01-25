3ba409e1ee78b163788597789be6bb23545453d2
dandihara <dandihara@gmail.com>
2022-01-25 18:23:14 +0900

    어드민 페이지 수정

--- rottenshoe/admin.py
+++ rottenshoe/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
-from .models import Sneakers,Keyword
+from .models import Sneakers,Keyword,User
 
 # Register your models here.
 
-
-class BoardAdmin(admin.ModelAdmin):
+class SneakerAdmin(admin.ModelAdmin):
     search_fields = ['sneaker_name','model_number']
+    list_display = ('sneaker_name','retail_date','brand','price')
 
 class KeywordAdmin(admin.ModelAdmin):
     search_fields = ['keyword']
+    list_display = ('keyword','sneaker_id')
 
-admin.site.register(Sneakers,BoardAdmin)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['email','nickname']
+    list_display = ('email','nickname','created_time','update_time')
+
+admin.site.register(Sneakers,SneakerAdmin)
 admin.site.register(Keyword,KeywordAdmin)
+admin.site.register(User,UserAdmin)
