1727e80c3951df6b5088af48c395f163d6193a67
Moon <gkaqhd0000@naver.com>
2022-02-15 23:13:55 +0900

    모델이름 반환코드 추가

--- rottenshoe_drf/admin.py
+++ rottenshoe_drf/admin.py
@@ -5,7 +5,11 @@ from models import SneakerFeatures
 # Register your models here.
 
 class SneakerReaturesAdmin(admin.ModelAdmin):
-    list_display = ('sneaker', 'comfortable','grip','spotlight','convenience')
+    list_display = ('sneaker_name', 'comfortable','grip','spotlight','convenience')
+
+    @admin.display(description="모델이름")
+    def sneaker_name(self,data):
+        return data.sneaker.sneaker_name
 
 
 admin.site.register(SneakerFeatures,SneakerReaturesAdmin)
